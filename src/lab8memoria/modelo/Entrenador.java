0757e40f098737cc55a4d21e810631b95b36891d
cuent <juan_valle@unitec.edu>
2026-09-12 09:15:11 -0600

    Clase Entrenador con su equipo y cambio de pokemon

---
+++ src/lab8memoria/modelo/Entrenador.java
@@ -0,0 +1,52 @@
+package lab8memoria.modelo;
+
+public class Entrenador {
+
+    private String nombre;
+    private ListaEnlazada equipo;
+
+    public Entrenador(String nombre) {
+        this.nombre = nombre;
+        this.equipo = new ListaEnlazada();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ListaEnlazada getEquipo() {
+        return equipo;
+    }
+
+    public void agregarPokemon(Pokemon pokemon) {
+        equipo.insertar(pokemon);
+    }
+
+    public Pokemon pokemonActivo() {
+        return equipo.obtenerActivo();
+    }
+
+    public boolean cambiarPokemon(String nombre) {
+        return equipo.establecerActivo(nombre);
+    }
+
+    public boolean continuarConSiguiente() {
+        return equipo.cambiarAlSiguienteDisponible();
+    }
+
+    public boolean tieneDisponibles() {
+        return equipo.contarDisponibles() > 0;
+    }
+
+    public int pokemonesDisponibles() {
+        return equipo.contarDisponibles();
+    }
+
+    public String toString() {
+        return nombre + " (" + equipo.contarDisponibles() + " de " + equipo.contar() + " disponibles)";
+    }
+}
